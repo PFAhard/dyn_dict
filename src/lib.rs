f8d7fb7f2fb65e1000db2ca25f0adf1c2f009af4
pavel <pasha-908@list.ru>
2021-08-16 13:40:25 +0300

    0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
 use std::any::Any;
+use serde::{Deserialize, Serialize};
 
-pub type Dict<'a> = HashMap<&'a str, Box<dyn Any>>;
+pub type Dict<'a> = HashMap<&'a str, Box<dyn AnySer<'a>>>;
+
+trait AnySer<'a>: Any + Deserialize<'a> + Serialize {}
